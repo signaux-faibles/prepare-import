b552c95cf721f6d45adbfae1e9b11a9da9127793
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-25 14:44:45 +0100

    refactor: split into sub-tests with t.Run()

--- main_test.go
+++ main_test.go
@@ -23,25 +23,42 @@ func serializeSlice(stringsSlice []string) string {
 }
 
 func TestPopulateFilesProperty(t *testing.T) {
-	var filesProperty FileProperty
-	filesProperty = PopulateFilesProperty([]string{"Sigfaibles_effectif_siret.csv"})
-	if filesProperty["effectif"][0] != "Sigfaibles_effectif_siret.csv" {
-		t.Error("PopulateFilesProperty should contain effectif file in \"effectif\" property")
-	}
 
-	filesProperty = PopulateFilesProperty([]string{"Sigfaibles_debits.csv"})
-	_, ok := filesProperty["debit"]
-	if !ok || filesProperty["debit"][0] != "Sigfaibles_debits.csv" {
-		t.Error("PopulateFilesProperty should contain one debit file in \"debit\" property")
-	}
+	/**
+	 * TIL: t.Run() can be used to define sub-tests. (see https://golang.org/pkg/testing/#hdr-Subtests_and_Sub_benchmarks)
+	 *
+	 * Here's what is logged by `$ go test` if the first t.Run() block fails:
+	 *
+	 * --- FAIL: TestPopulateFilesProperty (0.00s)
+	 *     --- FAIL: TestPopulateFilesProperty/PopulateFilesProperty_should_contain_effectif_file_in_"effectif"_property (0.00s)
+	 *         main_test.go:31: PopulateFilesProperty should contain effectif file in "effectif" property
+	 */
 
-	filesProperty = PopulateFilesProperty([]string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"})
-	_, ok = filesProperty["debit"]
-	if !ok ||
-		filesProperty["debit"][0] != "Sigfaibles_debits.csv" ||
-		filesProperty["debit"][1] != "Sigfaibles_debits2.csv" {
-		t.Error("PopulateFilesProperty should contain both debits files in \"debit\" property")
-	}
+	t.Run("PopulateFilesProperty should contain effectif file in \"effectif\" property", func(t *testing.T) {
+		var filesProperty FileProperty
+		filesProperty = PopulateFilesProperty([]string{"Sigfaibles_effectif_siret.csv"})
+		if filesProperty["effectif"][0] != "Sigfaibles_effectif_siret.csv" {
+			t.Error("PopulateFilesProperty should contain effectif file in \"effectif\" property")
+		}
+	})
+
+	t.Run("PopulateFilesProperty should contain one debit file in \"debit\" property", func(t *testing.T) {
+		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv"})
+		_, ok := filesProperty["debit"]
+		if !ok || filesProperty["debit"][0] != "Sigfaibles_debits.csv" {
+			t.Error("PopulateFilesProperty should contain one debit file in \"debit\" property")
+		}
+	})
+
+	t.Run("PopulateFilesProperty should contain both debits files in \"debit\" property", func(t *testing.T) {
+		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"})
+		_, ok := filesProperty["debit"]
+		if !ok ||
+			filesProperty["debit"][0] != "Sigfaibles_debits.csv" ||
+			filesProperty["debit"][1] != "Sigfaibles_debits2.csv" {
+			t.Error("PopulateFilesProperty should contain both debits files in \"debit\" property")
+		}
+	})
 }
 
 func TestGetFileType(t *testing.T) {
