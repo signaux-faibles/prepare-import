0098ce4a2df46c7566edd333ac7cbebe8fd6eeee
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-25 10:46:40 +0100

    detecting file types

--- main.go
+++ main.go
@@ -4,11 +4,23 @@
 package main
 
 func main(){
-
 }
-
 
 func PrepareImport() (string, error) {
 	// func Valid(data []byte) bool
 	return "{}", nil
+}
+
+func GetFileType(filename string) (string) {
+	switch filename {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+	return "effectif"
 }
--- main_test.go
+++ main_test.go
@@ -3,10 +3,22 @@
 package main
 
 import ("testing")
+
 // Prepare import should return json object.
 func TestPrepareImport(t *testing.T){
   res, _ := PrepareImport()
   if (res != "{}") {
     t.Error("Test failed: invalid json")
+  }
+}
+
+func TestGetFileType(t *testing.T){
+	res1 := GetFileType("Sigfaibles_effectif_siret.csv")
+	if (res1 != "effectif") {
+		t.Error("GetFileType should return \"effectif\" for \"Sigfaibles_effectif_siret.csv\"")
+	}
+	res2 := GetFileType("Sigfaibles_debits.csv")
+  if (res2 != "debit") {
+    t.Error("GetFileType should return \"debit\" for \"Sigfaibles_debits.csv\"")
   }
 }
