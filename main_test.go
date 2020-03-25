f13b34dbf9fcfe46f417d1f0fab14fb5c4c95830
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-25 15:16:15 +0100

    refactor(dry): extract isEqualSlice()

--- main_test.go
+++ main_test.go
@@ -16,6 +16,7 @@ func TestPrepareImport(t *testing.T) {
 	}
 }
 
+// To make slices of strings comparable.
 func serializeSlice(stringsSlice []string) string {
 	stringsClone := append(stringsSlice[:0:0], stringsSlice...)
 	sort.Strings(stringsClone)
@@ -32,45 +33,30 @@ func isEqual(t *testing.T, got, want string) {
 	}
 }
 
-// This function checks that a map's property is a slice of the expected length.
-func hasPropOfLength(t *testing.T, filesProperty FileProperty, propKey string, expectedLength int) {
+func isEqualSlice(t *testing.T, got, want []string) {
 	t.Helper()
-	if propValue, propExists := filesProperty[propKey]; !propExists {
-		t.Errorf("filesProperty should have a %q property", propKey)
-	} else if len(propValue) != expectedLength {
-		t.Errorf("filesProperty value has length %v instead of %v", len(propValue), expectedLength)
+	if serializeSlice(got) != serializeSlice(want) {
+		t.Errorf("got %q instead of %q", got, want)
 	}
 }
 
 func TestPopulateFilesProperty(t *testing.T) {
 
-	/**
-	 * t.Run() can be used to define sub-tests. (see https://golang.org/pkg/testing/#hdr-Subtests_and_Sub_benchmarks)
-	 *
-	 * Here's what is logged by `$ go test` if the first t.Run() block fails:
-	 *
-	 * --- FAIL: TestPopulateFilesProperty (0.00s)
-	 *     --- FAIL: TestPopulateFilesProperty/PopulateFilesProperty_should_contain_effectif_file_in_"effectif"_property (0.00s)
-	 *         main_test.go:31: PopulateFilesProperty should contain effectif file in "effectif" property
-	 */
+	// t.Run() is used to define sub-tests. (see https://golang.org/pkg/testing/#hdr-Subtests_and_Sub_benchmarks)
 
 	t.Run("PopulateFilesProperty should contain effectif file in \"effectif\" property", func(t *testing.T) {
 		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_effectif_siret.csv"})
-		hasPropOfLength(t, filesProperty, "effectif", 1)
-		isEqual(t, filesProperty["effectif"][0], "Sigfaibles_effectif_siret.csv")
+		isEqualSlice(t, filesProperty["effectif"], []string{"Sigfaibles_effectif_siret.csv"})
 	})
 
 	t.Run("PopulateFilesProperty should contain one debit file in \"debit\" property", func(t *testing.T) {
 		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv"})
-		hasPropOfLength(t, filesProperty, "debit", 1)
-		isEqual(t, filesProperty["debit"][0], "Sigfaibles_debits.csv")
+		isEqualSlice(t, filesProperty["debit"], []string{"Sigfaibles_debits.csv"})
 	})
 
 	t.Run("PopulateFilesProperty should contain both debits files in \"debit\" property", func(t *testing.T) {
 		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"})
-		hasPropOfLength(t, filesProperty, "debit", 2)
-		isEqual(t, filesProperty["debit"][0], "Sigfaibles_debits.csv")
-		isEqual(t, filesProperty["debit"][1], "Sigfaibles_debits2.csv")
+		isEqualSlice(t, filesProperty["debit"], []string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"})
 	})
 }
 
