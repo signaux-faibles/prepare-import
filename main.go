2dba705b9f5557d9a2f53614f59e3f7a4391d76c
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-26 10:10:54 +0100

    feat: Création de PopulateFilesProperty() avec tests (#4)
    `PopulateFilesProperty()` permet de générer la propriété `files` d'un batch qui sera stocké dans la base Admin.
    
    ```sh
    $ go test # pour exécuter les tests
    ```

---
+++ main.go
@@ -0,0 +1,46 @@
+// $ go build # to compile
+// $ ./prepare-import # to run
+
+package main
+
+import (
+	"errors"
+)
+
+func main() {
+}
+
+func PrepareImport() (string, error) {
+	// func Valid(data []byte) bool
+	return "{}", nil
+}
+
+type FileProperty map[string][]string
+
+func PopulateFilesProperty(filenames []string) FileProperty {
+	fileProperty := FileProperty{
+		// "effectif": []string{"coucou"},
+		// "debit":    []string{},
+	}
+	for _, filename := range filenames {
+		filetype, _ := GetFileType(filename)
+		if _, exists := fileProperty[filetype]; !exists {
+			fileProperty[filetype] = []string{}
+		}
+		fileProperty[filetype] = append(fileProperty[filetype], filename)
+	}
+	return fileProperty
+}
+
+func GetFileType(filename string) (string, error) {
+	switch filename {
+	case "Sigfaibles_effectif_siret.csv":
+		return "effectif", nil
+	case "Sigfaibles_debits.csv":
+		return "debit", nil
+	case "Sigfaibles_debits2.csv":
+		return "debit", nil
+	default:
+		return "", errors.New("Unrecognized type for " + filename)
+	}
+}
