90156c79833a76190bd07304b848ee68303d825c
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-03-30 11:22:36 +0200

    Factorisation de la création de fichiers temp

--- main_test.go
+++ main_test.go
@@ -19,107 +19,117 @@ import (
 // 	})
 // }
 
-func CreateTempDir() {
-	// see https://godoc.org/github.com/golang/dep/internal/test#Helper.TempDir
+func createTempFiles(t *testing.T, filename string) {
+  t.Helper()
+  dir, err := ioutil.TempDir(os.TempDir(), "example")
+  if err != nil {
+    t.Fatal(err.Error())
+  }
+  t.Cleanup(func() {os.RemoveAll(dir)} )
+
+  tmpFilename := filepath.Join(dir, filename)
+  if err := ioutil.WriteFile(tmpFilename, []byte{}, 0666); err != nil {
+    t.Fatal(err.Error())
+  }
 }
 
 // Test: ReadFilenames should return filenames in a directory
 func TestReadFilenames(t *testing.T) {
-	t.Run("Should return filenames in a directory", func(t *testing.T) {
-		dir, err := ioutil.TempDir(os.TempDir(), "example")
-		if err != nil {
-			t.Fatal(err.Error())
-		}
+  t.Run("Should return filenames in a directory", func(t *testing.T) {
+    dir, err := ioutil.TempDir(os.TempDir(), "example")
+    if err != nil {
+      t.Fatal(err.Error())
+    }
 
-		defer os.RemoveAll(dir) // clean up
+    defer os.RemoveAll(dir) // clean up
 
-		tmpFilename := filepath.Join(dir, "tmpfile")
-		if err := ioutil.WriteFile(tmpFilename, []byte{}, 0666); err != nil {
-			t.Fatal(err.Error())
-		}
+    tmpFilename := filepath.Join(dir, "tmpfile")
+    if err := ioutil.WriteFile(tmpFilename, []byte{}, 0666); err != nil {
+      t.Fatal(err.Error())
+    }
 
-		filenames, err := ReadFilenames(dir)
-		if err != nil {
-			t.Fatal(err.Error())
-		}
-		assert.Equal(t, []string{"tmpfile"}, filenames)
-	})
+    filenames, err := ReadFilenames(dir)
+    if err != nil {
+      t.Fatal(err.Error())
+    }
+    assert.Equal(t, []string{"tmpfile"}, filenames)
+  })
 }
 
 func TestPrepareImport(t *testing.T) {
-	t.Run("Should return a json with one file", func(t *testing.T) {
-		// Setup the test env
-		dir, err := ioutil.TempDir(os.TempDir(), "example")
-		if err != nil {
-			t.Fatal(err.Error())
-		}
+  t.Run("Should return a json with one file", func(t *testing.T) {
+    // Setup the test env
+    dir, err := ioutil.TempDir(os.TempDir(), "example")
+    if err != nil {
+      t.Fatal(err.Error())
+    }
 
-		defer os.RemoveAll(dir) // clean up
+    defer os.RemoveAll(dir) // clean up
 
-		tmpFilename := filepath.Join(dir, "Sigfaibles_debits.csv")
-		if err := ioutil.WriteFile(tmpFilename, []byte{}, 0666); err != nil {
-			t.Fatal(err.Error())
-		}
+    tmpFilename := filepath.Join(dir, "Sigfaibles_debits.csv")
+    if err := ioutil.WriteFile(tmpFilename, []byte{}, 0666); err != nil {
+      t.Fatal(err.Error())
+    }
 
-		// Run the actual test
-		res, _ := PrepareImport(dir)
-		expected := AdminObject{
-			"files": FileProperty{"debit": []string{"Sigfaibles_debits.csv"}},
-		}
-		assert.Equal(t, expected, res)
+    // Run the actual test
+    res, _ := PrepareImport(dir)
+    expected := AdminObject{
+      "files": FileProperty{"debit": []string{"Sigfaibles_debits.csv"}},
+    }
+    assert.Equal(t, expected, res)
 
-	})
+  })
 }
 
 // Prepare import should return json object.
 func TestPurePrepareImport(t *testing.T) {
-	t.Run("Should return a json with one file", func(t *testing.T) {
-		res, _ := PurePrepareImport([]string{"Sigfaibles_debits.csv"})
-		expected := AdminObject{
-			"files": FileProperty{"debit": []string{"Sigfaibles_debits.csv"}},
-		}
-		assert.Equal(t, expected, res)
-	})
-	t.Run("Should return an empty json when there is no file", func(t *testing.T) {
-		res, _ := PurePrepareImport([]string{})
-		assert.Equal(t, AdminObject{"files": FileProperty{}}, res)
-	})
+  t.Run("Should return a json with one file", func(t *testing.T) {
+    res, _ := PurePrepareImport([]string{"Sigfaibles_debits.csv"})
+    expected := AdminObject{
+      "files": FileProperty{"debit": []string{"Sigfaibles_debits.csv"}},
+    }
+    assert.Equal(t, expected, res)
+  })
+  t.Run("Should return an empty json when there is no file", func(t *testing.T) {
+    res, _ := PurePrepareImport([]string{})
+    assert.Equal(t, AdminObject{"files": FileProperty{}}, res)
+  })
 }
 
 func TestPopulateFilesProperty(t *testing.T) {
 
-	// t.Run() is used to define sub-tests. (see https://golang.org/pkg/testing/#hdr-Subtests_and_Sub_benchmarks)
+  // t.Run() is used to define sub-tests. (see https://golang.org/pkg/testing/#hdr-Subtests_and_Sub_benchmarks)
 
-	t.Run("PopulateFilesProperty should contain effectif file in \"effectif\" property", func(t *testing.T) {
-		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_effectif_siret.csv"})
-		// isEqualSlice(t, filesProperty["effectif"], []string{"Sigfaibles_effectif_siret.csv"})
-		assert.Equal(t, []string{"Sigfaibles_effectif_siret.csv"}, filesProperty["effectif"])
-	})
+  t.Run("PopulateFilesProperty should contain effectif file in \"effectif\" property", func(t *testing.T) {
+    filesProperty := PopulateFilesProperty([]string{"Sigfaibles_effectif_siret.csv"})
+    // isEqualSlice(t, filesProperty["effectif"], []string{"Sigfaibles_effectif_siret.csv"})
+    assert.Equal(t, []string{"Sigfaibles_effectif_siret.csv"}, filesProperty["effectif"])
+  })
 
-	t.Run("PopulateFilesProperty should contain one debit file in \"debit\" property", func(t *testing.T) {
-		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv"})
-		assert.Equal(t, []string{"Sigfaibles_debits.csv"}, filesProperty["debit"])
-	})
+  t.Run("PopulateFilesProperty should contain one debit file in \"debit\" property", func(t *testing.T) {
+    filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv"})
+    assert.Equal(t, []string{"Sigfaibles_debits.csv"}, filesProperty["debit"])
+  })
 
-	t.Run("PopulateFilesProperty should contain both debits files in \"debit\" property", func(t *testing.T) {
-		filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"})
-		assert.Equal(t, []string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"}, filesProperty["debit"])
-	})
+  t.Run("PopulateFilesProperty should contain both debits files in \"debit\" property", func(t *testing.T) {
+    filesProperty := PopulateFilesProperty([]string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"})
+    assert.Equal(t, []string{"Sigfaibles_debits.csv", "Sigfaibles_debits2.csv"}, filesProperty["debit"])
+  })
 }
 
 func TestGetFileType(t *testing.T) {
-	t.Run("should return \"effectif\" for \"Sigfaibles_effectif_siret.csv\"", func(t *testing.T) {
-		got, _ := GetFileType("Sigfaibles_effectif_siret.csv")
-		assert.Equal(t, "effectif", got)
-	})
+  t.Run("should return \"effectif\" for \"Sigfaibles_effectif_siret.csv\"", func(t *testing.T) {
+    got, _ := GetFileType("Sigfaibles_effectif_siret.csv")
+    assert.Equal(t, "effectif", got)
+  })
 
-	t.Run("should return \"debit\" for \"Sigfaibles_debits.csv\"", func(t *testing.T) {
-		got, _ := GetFileType("Sigfaibles_debits.csv")
-		assert.Equal(t, "debit", got)
-	})
+  t.Run("should return \"debit\" for \"Sigfaibles_debits.csv\"", func(t *testing.T) {
+    got, _ := GetFileType("Sigfaibles_debits.csv")
+    assert.Equal(t, "debit", got)
+  })
 
-	t.Run("should return \"debit\" for \"Sigfaibles_debits2.csv\"", func(t *testing.T) {
-		got, _ := GetFileType("Sigfaibles_debits2.csv")
-		assert.Equal(t, "debit", got)
-	})
+  t.Run("should return \"debit\" for \"Sigfaibles_debits2.csv\"", func(t *testing.T) {
+    got, _ := GetFileType("Sigfaibles_debits2.csv")
+    assert.Equal(t, "debit", got)
+  })
 }
