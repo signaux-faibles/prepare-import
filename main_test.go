9e969f111b1925758a9e1f91dcd594479e4b9f05
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-30 17:47:25 +0200

    red: effectif_dom.csv

--- main_test.go
+++ main_test.go
@@ -130,7 +130,7 @@ func TestGetFileType(t *testing.T) {
 	}{
 		{"diane_req_2002.csv", "diane"},
 		{"diane_req_dom_2002.csv", "diane"},
-		// {"effectif_dom.csv", "effectif"},
+		{"effectif_dom.csv", "effectif"},
 		// {"filter_siren_2002.csv", "filter"},
 		// {"sireneUL.csv", "sirene_ul"},
 		// {"StockEtablissement_utf8_geo.csv", "comptes"},
