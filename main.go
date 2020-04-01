8c991fafbc3dc941ba4efa0be39bd6fb4c4808e5
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-04-01 12:06:57 +0200

    refactor: Rerendre pures les fonctions pures + nommage + séparation fichier (#12)
    Création d'une interface `DataFile`, qui contient une méthode `DetectFileType`
    Sérialisation json depuis la commande prépare import.

--- main.go
+++ main.go
@@ -4,38 +4,85 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"path/filepath"
-	"regexp"
 	"strings"
+
+	"io/ioutil"
 )
 
 func main() {
-	// flags
 	var path = flag.String("path", ".", "Chemin d'accès aux fichiers données")
 	flag.Parse()
 	adminObject, err := PrepareImport(*path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(adminObject)
-	// TODO: serialize to JSON string
+	json, err := json.MarshalIndent(adminObject, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(json))
 }
 
 type AdminObject map[string]interface{}
+
+type DataFile interface {
+	GetFilename() string // the name as it will be stored in Admin
+	DetectFileType() string
+}
+
+type SimpleDataFile struct {
+	filename string
+}
+
+func (dataFile SimpleDataFile) DetectFileType() string {
+	return ExtractFileTypeFromFilename(dataFile.filename)
+}
+
+func (dataFile SimpleDataFile) GetFilename() string {
+	return dataFile.filename
+}
+
+type UploadedDataFile struct {
+	filename string
+	path     string
+}
+
+func (dataFile UploadedDataFile) DetectFileType() string {
+	metaFilepath := filepath.Join(dataFile.path, strings.Replace(dataFile.filename, ".bin", ".info", 1))
+	fileinfo, err := LoadMetadata(metaFilepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filetype := ExtractFileTypeFromMetadata(metaFilepath, fileinfo)
+	return filetype // e.g. "Sigfaible_debits.csv"
+}
+
+func (dataFile UploadedDataFile) GetFilename() string {
+	return dataFile.filename
+}
 
 func PrepareImport(pathname string) (AdminObject, error) {
 	filenames, err := ReadFilenames(pathname)
 	if err != nil {
 		return nil, err
 	}
-	return PurePrepareImport(filenames, pathname), nil
+	augmentedFiles := []DataFile{}
+	for _, file := range filenames {
+		var filename DataFile
+		if strings.HasSuffix(file, ".bin") {
+			filename = UploadedDataFile{file, pathname}
+		} else {
+			filename = SimpleDataFile{file}
+		}
+		augmentedFiles = append(augmentedFiles, filename)
+	}
+	return PurePrepareImport(augmentedFiles), nil
 }
 
-// TODO: PurePrepareImport is not pure anymore, because it takes a path, and can indirectly read files
-func PurePrepareImport(filenames []string, path string) AdminObject {
-	filesProperty := PopulateFilesProperty(filenames, path)
+func PurePrepareImport(augmentedFilenames []DataFile) AdminObject {
+	filesProperty := PopulateFilesProperty(augmentedFilenames)
 	return AdminObject{"files": filesProperty}
 }
 
@@ -71,23 +118,11 @@ func LoadMetadata(filepath string) (UploadedFileMeta, error) {
 	return uploadedFileMeta, nil
 }
 
-func PopulateFilesProperty(filenames []string, path string) FilesProperty {
-	filesProperty := FilesProperty{
-		// "effectif": []string{"coucou"},
-		// "debit":    []string{},
-	}
+func PopulateFilesProperty(filenames []DataFile) FilesProperty {
+	filesProperty := FilesProperty{}
 	for _, filename := range filenames {
-		var filetype string
-		if strings.HasSuffix(filename, ".bin") {
-			metaFilepath := filepath.Join(path, strings.Replace(filename, ".bin", ".info", 1))
-			fileinfo, err := LoadMetadata(metaFilepath)
-			if err != nil {
-				log.Fatal(err)
-			}
-			filetype = GetFileTypeFromMetadata(filename, fileinfo)
-		} else {
-			filetype = GetFileType(filename)
-		}
+		filetype := filename.DetectFileType()
+
 		if filetype == "" {
 			// Unsupported file
 			continue
@@ -95,65 +130,7 @@ func PopulateFilesProperty(filenames []string, path string) FilesProperty {
 		if _, exists := filesProperty[filetype]; !exists {
 			filesProperty[filetype] = []string{}
 		}
-		filesProperty[filetype] = append(filesProperty[filetype], filename)
+		filesProperty[filetype] = append(filesProperty[filetype], filename.GetFilename())
 	}
 	return filesProperty
-}
-
-var hasDianePrefix = regexp.MustCompile(`^diane`)
-var mentionsEffectif = regexp.MustCompile(`effectif_`)
-var mentionsDebits = regexp.MustCompile(`_debits`)
-var hasFilterPrefix = regexp.MustCompile(`^filter_`)
-
-type MetadataProperty map[string]string
-
-type UploadedFileMeta struct {
-	MetaData MetadataProperty
-}
-
-func GetFileTypeFromMetadata(filename string, fileinfo UploadedFileMeta) string {
-	metadata := fileinfo.MetaData
-	if metadata["goup-path"] == "bdf" {
-		return "bdf"
-	} else {
-		return GetFileType(metadata["filename"])
-	}
-}
-
-// GetFileType returns a file type from filename, or empty string for unsupported file names
-func GetFileType(filename string) string {
-	switch {
-	case filename == "act_partielle_conso_depuis2014_FRANCE.csv":
-		return "apconso"
-	case filename == "act_partielle_ddes_depuis2015_FRANCE.csv":
-		return "apdemande"
-	case filename == "Sigfaible_etablissement_utf8.csv":
-		return "admin_urssaf"
-	case filename == "Sigfaible_effectif_siren.csv":
-		return "effectif_ent"
-	case filename == "Sigfaible_pcoll.csv":
-		return "procol"
-	case filename == "Sigfaible_cotisdues.csv":
-		return "cotisation"
-	case filename == "Sigfaible_delais.csv":
-		return "delai"
-	case filename == "Sigfaible_ccsf.csv":
-		return "ccsf"
-	case filename == "sireneUL.csv":
-		return "sirene_ul"
-	case filename == "StockEtablissement_utf8_geo.csv":
-		return "comptes"
-	case strings.HasSuffix(filename, ".sas7bdat"):
-		return "interim"
-	case mentionsDebits.MatchString(filename):
-		return "debit"
-	case hasDianePrefix.MatchString(filename):
-		return "diane"
-	case mentionsEffectif.MatchString(filename):
-		return "effectif"
-	case hasFilterPrefix.MatchString(filename):
-		return "filter"
-	default:
-		return ""
-	}
 }
---
+++ signauxfaiblesTypes.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+  "regexp"
+  "strings"
+)
+
+var hasDianePrefix = regexp.MustCompile(`^diane`)
+var mentionsEffectif = regexp.MustCompile(`effectif_`)
+var mentionsDebits = regexp.MustCompile(`_debits`)
+var hasFilterPrefix = regexp.MustCompile(`^filter_`)
+
+type MetadataProperty map[string]string
+
+type UploadedFileMeta struct {
+	MetaData MetadataProperty
+}
+
+func ExtractFileTypeFromMetadata(filename string, fileinfo UploadedFileMeta) string {
+	metadata := fileinfo.MetaData
+	if metadata["goup-path"] == "bdf" {
+		return "bdf"
+	} else {
+		return ExtractFileTypeFromFilename(metadata["filename"])
+	}
+}
+
+// ExtractFileTypeFromFilename returns a file type from filename, or empty string for unsupported file names
+func ExtractFileTypeFromFilename(filename string) string {
+	switch {
+	case filename == "act_partielle_conso_depuis2014_FRANCE.csv":
+		return "apconso"
+	case filename == "act_partielle_ddes_depuis2015_FRANCE.csv":
+		return "apdemande"
+	case filename == "Sigfaible_etablissement_utf8.csv":
+		return "admin_urssaf"
+	case filename == "Sigfaible_effectif_siren.csv":
+		return "effectif_ent"
+	case filename == "Sigfaible_pcoll.csv":
+		return "procol"
+	case filename == "Sigfaible_cotisdues.csv":
+		return "cotisation"
+	case filename == "Sigfaible_delais.csv":
+		return "delai"
+	case filename == "Sigfaible_ccsf.csv":
+		return "ccsf"
+	case filename == "sireneUL.csv":
+		return "sirene_ul"
+	case filename == "StockEtablissement_utf8_geo.csv":
+		return "comptes"
+	case strings.HasSuffix(filename, ".sas7bdat"):
+		return "interim"
+	case mentionsDebits.MatchString(filename):
+		return "debit"
+	case hasDianePrefix.MatchString(filename):
+		return "diane"
+	case mentionsEffectif.MatchString(filename):
+		return "effectif"
+	case hasFilterPrefix.MatchString(filename):
+		return "filter"
+	default:
+		return ""
+	}
+}
