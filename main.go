f43a49e4f9fe26a217945ba5e5195ace6edf9721
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-02 13:49:18 +0200

    tentative: ajout de point à chaque commentaire godoc cf https://app.codacy.com/gh/signaux-faibles/prepare-import/pullRequest?prid=5292537

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"io/ioutil"
 )
 
-// Implementation of the prepare-import command
+// Implementation of the prepare-import command.
 func main() {
 	var path = flag.String("path", ".", "Chemin d'accès aux fichiers données")
 	flag.Parse()
@@ -26,19 +26,19 @@ func main() {
 	fmt.Println(string(json))
 }
 
-// AdminObject represents a document going to be stored in the Admin db collection
+// AdminObject represents a document going to be stored in the Admin db collection.
 type AdminObject map[string]interface{}
 
-// FilesProperty represents the "files" property of an Admin object
+// FilesProperty represents the "files" property of an Admin object.
 type FilesProperty map[string][]string
 
-// DataFile represents a Data File to be imported, and allows to determine its type and name
+// DataFile represents a Data File to be imported, and allows to determine its type and name.
 type DataFile interface {
 	GetFilename() string    // the name as it will be stored in Admin
 	DetectFileType() string // returns the type of that file (e.g. "debit")
 }
 
-// SimpleDataFile is a DataFile which type can be determined without requiring a metadata file (e.g. well-named csv file)
+// SimpleDataFile is a DataFile which type can be determined without requiring a metadata file (e.g. well-named csv file).
 type SimpleDataFile struct {
 	filename string
 }
@@ -51,7 +51,7 @@ func (dataFile SimpleDataFile) GetFilename() string {
 	return dataFile.filename
 }
 
-// UploadedDataFile is a DataFile which type can be determined thanks to a metadata file (e.g. bin+info files)
+// UploadedDataFile is a DataFile which type can be determined thanks to a metadata file (e.g. bin+info files).
 type UploadedDataFile struct {
 	filename string
 	path     string
@@ -70,7 +70,7 @@ func (dataFile UploadedDataFile) GetFilename() string {
 	return dataFile.filename
 }
 
-// AugmentDataFile returns a SimpleDataFile or UploadedDataFile (if metadata had to be loaded)
+// AugmentDataFile returns a SimpleDataFile or UploadedDataFile (if metadata had to be loaded).
 func AugmentDataFile(file string, pathname string) DataFile {
 	if strings.HasSuffix(file, ".bin") {
 		return UploadedDataFile{file, pathname}
@@ -78,7 +78,7 @@ func AugmentDataFile(file string, pathname string) DataFile {
 	return SimpleDataFile{file}
 }
 
-// PrepareImport generates an Admin object from files found at given pathname of the file system
+// PrepareImport generates an Admin object from files found at given pathname of the file system.
 func PrepareImport(pathname string) (AdminObject, error) {
 	filenames, err := ReadFilenames(pathname)
 	if err != nil {
@@ -91,13 +91,13 @@ func PrepareImport(pathname string) (AdminObject, error) {
 	return PurePrepareImport(augmentedFiles), nil
 }
 
-// PurePrepareImport populates an AdminObject, given a list of data files
+// PurePrepareImport populates an AdminObject, given a list of data files.
 func PurePrepareImport(augmentedFilenames []DataFile) AdminObject {
 	filesProperty := PopulateFilesProperty(augmentedFilenames)
 	return AdminObject{"files": filesProperty}
 }
 
-// ReadFilenames returns the name of files found at the provided path
+// ReadFilenames returns the name of files found at the provided path.
 func ReadFilenames(path string) ([]string, error) {
 	var files []string
 	fileInfo, err := ioutil.ReadDir(path)
@@ -110,7 +110,7 @@ func ReadFilenames(path string) ([]string, error) {
 	return files, nil
 }
 
-// LoadMetadata returns the metadata of a .bin file, by reading the given .info file
+// LoadMetadata returns the metadata of a .bin file, by reading the given .info file.
 func LoadMetadata(filepath string) (UploadedFileMeta, error) {
 
 	// read file
@@ -129,7 +129,7 @@ func LoadMetadata(filepath string) (UploadedFileMeta, error) {
 	return uploadedFileMeta, nil
 }
 
-// PopulateFilesProperty populates the "files" property of an Admin object, given a list of Data files
+// PopulateFilesProperty populates the "files" property of an Admin object, given a list of Data files.
 func PopulateFilesProperty(filenames []DataFile) FilesProperty {
 	filesProperty := FilesProperty{}
 	for _, filename := range filenames {
