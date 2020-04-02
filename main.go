095ec3598be23954a6b3f4a9085607131dfb99fe
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-02 09:45:25 +0200

    refactor: réduction de complexité + commentaires cf https://github.com/signaux-faibles/prepare-import/pull/12/files#r401497105

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"io/ioutil"
 )
 
+// Implementation of the prepare-import command
 func main() {
 	var path = flag.String("path", ".", "Chemin d'accès aux fichiers données")
 	flag.Parse()
@@ -25,13 +26,19 @@ func main() {
 	fmt.Println(string(json))
 }
 
+// Type of the Admin object (as stored in the database)
 type AdminObject map[string]interface{}
 
+// Type of the "files" property of an Admin object
+type FilesProperty map[string][]string
+
+// Interface representing a Data File, and allows to determine its type and name
 type DataFile interface {
-	GetFilename() string // the name as it will be stored in Admin
-	DetectFileType() string
+	GetFilename() string    // the name as it will be stored in Admin
+	DetectFileType() string // returns the type of that file (e.g. "debit")
 }
 
+// DataFile which type can be determined without requiring a metadata file (e.g. well-named csv file)
 type SimpleDataFile struct {
 	filename string
 }
@@ -44,6 +51,7 @@ func (dataFile SimpleDataFile) GetFilename() string {
 	return dataFile.filename
 }
 
+// DataFile which type can be determined thanks to a metadata file (e.g. bin+info files)
 type UploadedDataFile struct {
 	filename string
 	path     string
@@ -55,14 +63,23 @@ func (dataFile UploadedDataFile) DetectFileType() string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	filetype := ExtractFileTypeFromMetadata(metaFilepath, fileinfo)
-	return filetype // e.g. "Sigfaible_debits.csv"
+	return ExtractFileTypeFromMetadata(metaFilepath, fileinfo) // e.g. "Sigfaible_debits.csv"
 }
 
 func (dataFile UploadedDataFile) GetFilename() string {
 	return dataFile.filename
 }
 
+// Returns a SimpleDataFile or UploadedDataFile (if metadata had to be loaded)
+func AugmentDataFile(file string, pathname string) DataFile {
+	if strings.HasSuffix(file, ".bin") {
+		return UploadedDataFile{file, pathname}
+	} else {
+		return SimpleDataFile{file}
+	}
+}
+
+// Generates an Admin object from files found at given pathname of the file system
 func PrepareImport(pathname string) (AdminObject, error) {
 	filenames, err := ReadFilenames(pathname)
 	if err != nil {
@@ -70,22 +87,18 @@ func PrepareImport(pathname string) (AdminObject, error) {
 	}
 	augmentedFiles := []DataFile{}
 	for _, file := range filenames {
-		var filename DataFile
-		if strings.HasSuffix(file, ".bin") {
-			filename = UploadedDataFile{file, pathname}
-		} else {
-			filename = SimpleDataFile{file}
-		}
-		augmentedFiles = append(augmentedFiles, filename)
+		augmentedFiles = append(augmentedFiles, AugmentDataFile(file, pathname))
 	}
 	return PurePrepareImport(augmentedFiles), nil
 }
 
+// Populates an AdminObject, given a list of data files
 func PurePrepareImport(augmentedFilenames []DataFile) AdminObject {
 	filesProperty := PopulateFilesProperty(augmentedFilenames)
 	return AdminObject{"files": filesProperty}
 }
 
+// Returns the name of files found at the provided path
 func ReadFilenames(path string) ([]string, error) {
 	var files []string
 	fileInfo, err := ioutil.ReadDir(path)
@@ -98,8 +111,7 @@ func ReadFilenames(path string) ([]string, error) {
 	return files, nil
 }
 
-type FilesProperty map[string][]string
-
+// Returns the metadata of a .bin file, from the given .info file.
 func LoadMetadata(filepath string) (UploadedFileMeta, error) {
 
 	// read file
@@ -118,6 +130,7 @@ func LoadMetadata(filepath string) (UploadedFileMeta, error) {
 	return uploadedFileMeta, nil
 }
 
+// Populates the "files" property of an Admin object, given a list of Data files
 func PopulateFilesProperty(filenames []DataFile) FilesProperty {
 	filesProperty := FilesProperty{}
 	for _, filename := range filenames {
