2c4a335ee24990cd7196f3160064dbd997650b9d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-07 15:40:01 +0200

    🔵 fix createTempFiles() to create more than one file

--- end_to_end_test.go
+++ end_to_end_test.go
@@ -13,19 +13,18 @@ import (
 func TestMain(t *testing.T) {
 	t.Run("prepare-import golden file", func(t *testing.T) {
 
-		dir := createTempFiles(t, "Sigfaibles_effectif_siret.csv")
-		createTempFiles(t, "Sigfaibles_debits.csv")
+		dir := createTempFiles(t, []string{"Sigfaibles_effectif_siret.csv", "Sigfaibles_debits.csv"})
 
 		cmd := exec.Command("./prepare-import", "--path", dir)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		err := cmd.Run()
 
-		fmt.Printf("in all caps: %q\n", out.String())
+		fmt.Printf("stdout: %q\n", out.String())
 		if err != nil {
 			log.Fatal(err)
 		}
-		expected := "{\n  \"files\": {\n    \"effectif\": [\n      \"Sigfaibles_effectif_siret.csv\"\n    ]\n  }\n}\n"
+		expected := "{\n  \"files\": {\n    \"debit\": [\n      \"Sigfaibles_debits.csv\"\n    ],\n    \"effectif\": [\n      \"Sigfaibles_effectif_siret.csv\"\n    ]\n  }\n}\n"
 		assert.Equal(t, expected, out.String())
 	})
 }
