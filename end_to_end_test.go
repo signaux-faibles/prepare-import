f51b376ace85014252e517f17abe38f314dae324
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-04-07 15:35:04 +0200

    Nettoyage test e2e

--- end_to_end_test.go
+++ end_to_end_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os/exec"
 	"testing"
@@ -13,12 +14,14 @@ func TestMain(t *testing.T) {
 	t.Run("prepare-import golden file", func(t *testing.T) {
 
 		dir := createTempFiles(t, "Sigfaibles_effectif_siret.csv")
+		createTempFiles(t, "Sigfaibles_debits.csv")
 
 		cmd := exec.Command("./prepare-import", "--path", dir)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		err := cmd.Run()
 
+		fmt.Printf("in all caps: %q\n", out.String())
 		if err != nil {
 			log.Fatal(err)
 		}
