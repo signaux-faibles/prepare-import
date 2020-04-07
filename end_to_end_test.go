0a5e15344ed728463e9f80c361ada33172066c5a
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-04-07 15:28:26 +0200

    Test OK du binaire

--- end_to_end_test.go
+++ end_to_end_test.go
@@ -2,34 +2,27 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"os/exec"
-	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(t *testing.T) {
 	t.Run("prepare-import golden file", func(t *testing.T) {
 
-		// dir := createTempFiles(t, "tmpfile.csv")
 		dir := createTempFiles(t, "Sigfaibles_effectif_siret.csv")
 
 		cmd := exec.Command("./prepare-import", "--path", dir)
-		cmd.Stdin = strings.NewReader("some input")
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		err := cmd.Run()
 
-		fmt.Printf("in all caps: %q\n", out.String())
 		if err != nil {
 			log.Fatal(err)
 		}
-		// https://golang.org/pkg/os/exec/#Command
-
-		// filesProperty, unsupportedFiles := PopulateFilesProperty([]DataFile{SimpleDataFile{"Sigfaibles_effectif_siret.csv"}})
-		// if assert.Len(t, unsupportedFiles, 0) {
-		// 	assert.Equal(t, []string{"Sigfaibles_effectif_siret.csv"}, filesProperty["effectif"])
-		// }
+		expected := "{\n  \"files\": {\n    \"effectif\": [\n      \"Sigfaibles_effectif_siret.csv\"\n    ]\n  }\n}\n"
+		assert.Equal(t, expected, out.String())
 	})
 }
