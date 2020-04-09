391e77648c1edaad57f2b25212b6a92da4ff14ad
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-09 10:45:33 +0200

    🔴PopulateAdminObject Should return a date_fin consistent with batch key

--- main_test.go
+++ main_test.go
@@ -163,6 +163,13 @@ func TestPopulateAdminObject(t *testing.T) {
 		}
 	})
 
+	t.Run("Should return a date_fin consistent with batch key", func(t *testing.T) {
+		res, err := PopulateAdminObject([]DataFile{}, "1912") // ~= 12/2019
+		expected := map[string]string{"$date": "2019-12-01T00:00:00.000+0000"}
+		if assert.NoError(t, err) {
+			assert.Equal(t, expected, res["param"].(map[string]map[string]string)["date_fin"])
+		}
+	})
 }
 
 func TestPopulateFilesProperty(t *testing.T) {
