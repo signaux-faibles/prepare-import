d313f2049d0973b4fc79385350d696de255a1cf7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-10 19:36:08 +0100

    ❌ test: prévenir la présence de plus d'1 fichier effectif

--- prepareimport/prepareimport_test.go
+++ prepareimport/prepareimport_test.go
@@ -30,6 +30,13 @@ func TestPrepareImport(t *testing.T) {
 		assert.Equal(t, expected, err.Error())
 	})
 
+	t.Run("Should warn if 2 effectif files are provided", func(t *testing.T) {
+		dir := CreateTempFiles(t, dummyBatchKey, []string{"Sigfaible_effectif_siret.csv", "Sigfaible_effectif_siret2.csv"})
+		_, err := PrepareImport(dir, dummyBatchKey, dummyDateFinEffectif)
+		expected := "generating a filter requires just 1 effectif file, found: [/1802/Sigfaible_effectif_siret.csv /1802/Sigfaible_effectif_siret2.csv]"
+		assert.Equal(t, expected, err.Error())
+	})
+
 	t.Run("Should warn if neither effectif and date_fin_effectif are provided", func(t *testing.T) {
 		dir := CreateTempFiles(t, dummyBatchKey, []string{"filter_2002.csv"})
 		_, err := PrepareImport(dir, dummyBatchKey, "")
