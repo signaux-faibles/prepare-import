efaebea430c3115649d2cb484d6c93cfc67cf391
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-17 15:03:40 +0100

    fix: usage du format xlsx au lieu de xls

--- tools/filter_to_diane_test.go
+++ tools/filter_to_diane_test.go
@@ -38,17 +38,17 @@ func TestFilterToDiane(t *testing.T) {
 func TestFilterToDianeXls(t *testing.T) {
 	t.Run("filter_to_diane to MS Excel golden file", func(t *testing.T) {
 
-		cmd := exec.Command("ssconvert", "filter_to_diane_golden.txt", "filter_to_diane_golden_tmp.xls")
+		cmd := exec.Command("ssconvert", "filter_to_diane_golden.txt", "filter_to_diane_golden_tmp.xlsx")
 		err := cmd.Run()
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		t.Cleanup(func() {
-			_ = os.Remove("filter_to_diane_golden_tmp.xls")
+			_ = os.Remove("filter_to_diane_golden_tmp.xlsx")
 		})
 
-		file, err := os.Open("filter_to_diane_golden_tmp.xls")
+		file, err := os.Open("filter_to_diane_golden_tmp.xlsx")
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -59,7 +59,7 @@ func TestFilterToDianeXls(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		cmd = exec.Command("ssconvert", "filter_to_diane_golden_tmp.xls", "filter_to_diane_golden_tmp_back.csv")
+		cmd = exec.Command("ssconvert", "filter_to_diane_golden_tmp.xlsx", "filter_to_diane_golden_tmp_back.csv")
 		err = cmd.Run()
 		if err != nil {
 			t.Fatal(err)
