c0b7b7815d18f6dabfdee02ad6791450c5329970
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-02-05 15:08:55 +0100

    fix: respect du nom de fichier initial dans test e2e

--- main_test.go
+++ main_test.go
@@ -32,9 +32,11 @@ func TestMain(t *testing.T) {
 			"Sigfaibles_effectif_siret.csv": effectifData,
 			"Sigfaibles_debits.csv":         {},
 			"abcdef":                        {},
-			"abcdef.info":                   []byte(`{ "MetaData": { "filename": "Sigfaible_pcoll.csv", "goup-path": "bdf" } }`),
+			"abcdef.info":                   []byte(`{ "MetaData": { "filename": "FICHIER_SF_2020_02.csv", "goup-path": "bdf" } }`),
 			"unsupported.csv":               {},
 			"E_202011095813_Retro-Paydex_20201207.csv": {},
+			// "083fe617e80f2e30a21598d38a854bc6":         {},
+			// "083fe617e80f2e30a21598d38a854bc6.info":    []byte(`{ "MetaData": { "filename": "Sigfaible_pcoll.csv", "goup-path": "" } }`),
 		})
 
 		cmds := []*exec.Cmd{
